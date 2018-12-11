66e5612f52d792b07fce03c2f1d6fb3ef1c89215
aneri2610 <anerishah36@gmail.com>
2018-12-11 18:27:15 +0530

    add comlexity limit

--- server/server.go
+++ server/server.go
@@ -16,10 +16,10 @@ func main() {
 	if port == "" {
 		port = defaultPort
 	}
-
+	c := graphql.Config{Resolvers: &graphql.Resolver{}}
 	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
-	http.Handle("/query", handler.GraphQL(graphql.NewExecutableSchema(graphql.Config{Resolvers: &graphql.Resolver{}})))
-
+	http.Handle("/query", handler.GraphQL(graphql.NewExecutableSchema(c)))
+	//http.Handle("/query", handler.GraphQL(graphql.NewExecutableSchema(c), handler.ComplexityLimit(5))) // To limit complexity to 5
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
